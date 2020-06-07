c7e62f308548609d4a2b1629f389b26f1920a8b9
mashreqsol <47236439+mashreqsol@users.noreply.github.com>
2020-06-07 17:03:16 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 import Hello from './Hello'
 
-const App =({name,SirName,age}) =>  <div className="App"> <Hello SirName = {SirName}> </Hello> <h1> Pakistan Zindabad!!! Name: { name }  </h1> <h2> Pakistan Zindabad!!! Age: { age - 11 }</h2><h3> Pakistan Zindabad!!! </h3></div>
+const App =({name,SirName,age}) =>  <div className="App"> <Hello SirName = {SirName}> </Hello> <h2> Name: { name }  </h2> <h2> Age: { age }</h2><h1> Pakistan Zindabad!!! </h1></div>
  
 
 
