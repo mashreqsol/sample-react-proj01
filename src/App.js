c37d59e45d4923cbc7fb6a14e406292569a7fbba
mashreqsol <47236439+mashreqsol@users.noreply.github.com>
2020-06-07 16:50:07 +0500

    Steps Done After Doing English Lecture 03

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 import React from 'react';
 import './App.css';
+import Hello from './Hello'
 
-function App() {
-  return (
-    <div className="App">
-     <h1> Pakistan Zindabad!!! </h1>
-    </div>
-  );
-}
+const App =({name,SirName,age}) =>  <div className="App"> <Hello SirName = {SirName}> </Hello> <h1> Pakistan Zindabad!!! Name: { name }  </h1> <h2> Pakistan Zindabad!!! Age: { age - 11 }</h2><h3> Pakistan Zindabad!!! </h3></div>
+ 
+
 
 export default App;
---
+++ src/Hello.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './Hello.css'
+
+const Hello =({SirName}) =>  <div className="Hello"> <p className="myname"> Sir Name : {SirName} </p></div>
+ 
+
+
+export default Hello;
